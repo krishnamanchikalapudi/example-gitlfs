97a889d7863e954df9f3569b63887485032e192a
Krishna Manchikalapudi <krishnam@jfrog.com>
2024-11-22 13:33:03 -0800

    testing Git LFS 2024-11-22-13-33

--- git-lfs.sh
+++ git-lfs.sh
@@ -1,15 +1,19 @@
 echo "\n\n**** GIT-LFS.SH - STARTed at $(date '+%Y-%m-%d-%H-%M') ****\n\n"
 set -x # activate debugging from here
 
-echo " GIT-LFS:  $(git lfs version)"
+echo "\n GIT-LFS:  $(git lfs version)"
 
+echo "\n"
 git lfs track "target/*.*"
 
+echo "\n"
 git add . -v
 # git add target/*.*
 
+echo "\n"
 git commit -m "testing Git LFS $(date '+%Y-%m-%d-%H-%M') "
 
+echo "\n"
 git push -v
 
 set +x # stop debugging from here
